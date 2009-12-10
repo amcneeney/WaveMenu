0fb969b521dda9b2a6a062e15cade6195a0d08e1
Adam McNeeney <adam@transitive.com>
2009-12-10 14:24:10 +0000

    Initial empty project.

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  WaveMenu
+//
+//  Created by Adam McNeeney on 10/12/2009.
+//  Copyright Transitive Ltd. 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
